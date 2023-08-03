c43b84beb61aafec0992f42196965b7169b162b1
Nsalex18 <nicolassimoes861@gmail.com>
2023-08-02 22:11:50 -0300

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function generateNumber() {
 
-const min = Math.ceil (document.querySelector ('.Input-min').value)
-const max = Math.floor (document.querySelector ('.Input-max').value)
+    const min = Math.ceil(document.querySelector('.Input-min').value)
+    const max = Math.floor(document.querySelector('.Input-max').value)
 
-const result = Math.floor (Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert('O valor mínimo deve ser MENOR que o valor máximo')
+    } 
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-alert(result)
+        alert(result)
+
+    }
 
 }
+
+// Poderiamos alterar a lógica também.
